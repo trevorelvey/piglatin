02561e0e9690488eb035e8bd734f0be71c9272e1
Trevor Elvey and Ramon de la Cruz <trevor.elvey@gmail.com>
2016-01-20 13:49:11 -0800

    added script to translate words that start with multiple consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var vowels = ['a', 'e', 'i', 'o', 'u'];
 var ay = ['ay']
+var qu = ['qu']
 
 var vowelCheck = function(word) {
 
@@ -15,9 +16,19 @@ var translate = function(word) {
   if (vowels.includes(word[0])) {
     return (word.concat(ay));
 
+  // } else if (!vowels.includes(word[0])){
+  //   return (word.substring(1,word.length) + word.substring(0,1) + ay);
+  } else if (vowels.includes(word[1])){
+    return (word.substring(1,word.length) + word.substring(0,1) + ay);
+  } else if (vowels.includes(word[2])){
+    return (word.substring(2,word.length) + word.substring(0,2) + ay);
+  } else if (vowels.includes(word[3])){
+    return (word.substring(3,word.length) + word.substring(0,3) + ay);
+  } else if (vowels.includes(word[4])){
+    return (word.substring(4,word.length) + word.substring(0,4) + ay);
   } else {
-    return (word.concat(word[0]).concat(ay));
-  }
+    return false;
+  };
 
   //
   // else if word = word.substring(1) {
--- spec/specs.js
+++ spec/specs.js
@@ -12,6 +12,26 @@ describe('vowelCheck', function() {
   });
 
   it("moves the first consonant to the end of the word and adds 'ay'", function() {
-    expect(translate("table")).to.equal("tabletay")
+    expect(translate("table")).to.equal("abletay")
+  });
+
+  it("moves the first and second consonant to the end of the word and adds 'ay'", function() {
+    expect(translate("stick")).to.equal("ickstay")
+  });
+  it("moves the first, second, and third consonants to the end of the word and adds 'ay'", function() {
+    expect(translate("string")).to.equal("ingstray")
+  });
+  it("moves the first, second, third, and fourth consonants to the end of the word and adds 'ay'", function() {
+    expect(translate("tttta")).to.equal("attttay")
+  });
+  it("moves the consonants up to and including 'qu' to the end of the word and adds 'ay'", function(){
+    expect(translate("squeal")).to.equal("ealsquay")
   });
 });
+//
+//
+// potato
+// subString[1,string.length] + subString[0,1] + "ay"
+//
+// dream
+// subString[2,string.length] + subSTring[0,2] + "ay"
